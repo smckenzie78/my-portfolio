ecc2ac0c676de95252aaefcef2a16683d0ae21b8
smckenzie78 <109680885+smckenzie78@users.noreply.github.com>
2025-06-08 17:51:59 -0400

    reverting navbar back to unresponsive design

--- src/app/_components/navbar/navbar.tsx
+++ src/app/_components/navbar/navbar.tsx
@@ -6,21 +6,21 @@ import './navbar.css'
 
 export default function Navbar(){
     return(
-        <div className="flex lg:h-screen w-full gap-20 lg:flex-col justify-center lg:items-center items-end">
+        <div className="flex h-screen w-full gap-20  flex-col justify-center  items-center">
             <Link href={{pathname: './'}}>
-                <div className={`buttonclass bg-cyan-600 p-4 rounded-full text-2xl flex items-center justify-between h-[50px] w-[200px] text-gray-200 hover:text-white lg:absolute right-0`}>
+                <div className={`buttonclass bg-cyan-600 p-4 rounded-full text-2xl flex items-center justify-between h-[50px] w-[200px] text-gray-200 hover:text-white absolute right-0`}>
                     <span className={`font-[600] text-sm uppercase`}>Home</span>
                     <FontAwesomeIcon icon={faHouse} className='icon'/>
                 </div>
             </Link>
             <Link href={{pathname: './about'}}>
-                <div className={`buttonclass bg-cyan-600 p-4 rounded-full text-2xl flex items-center justify-between h-[50px] w-[200px] text-gray-200 hover:text-white lg:absolute right-0`}>
+                <div className={`buttonclass bg-cyan-600 p-4 rounded-full text-2xl flex items-center justify-between h-[50px] w-[200px] text-gray-200 hover:text-white absolute right-0`}>
                     <span className={`font-[600] text-sm uppercase`}>About</span>
                     <FontAwesomeIcon icon={faUser} />
                 </div>
             </Link>
             <Link href={{pathname: './contact'}}>
-                <div className={`buttonclass bg-cyan-600 p-4 rounded-full text-2xl flex items-center justify-between h-[50px] w-[200px] text-gray-200 hover:text-white lg:absolute right-0`}>
+                <div className={`buttonclass bg-cyan-600 p-4 rounded-full text-2xl flex items-center justify-between h-[50px] w-[200px] text-gray-200 hover:text-white absolute right-0`}>
                     <span className={`font-[600] text-sm uppercase`}>Contact</span>
                     <FontAwesomeIcon icon={faPaperPlane} />
                 </div>
