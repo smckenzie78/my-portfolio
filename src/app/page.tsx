a34ab4761bc0df33ff4a507b56b162b9a4950b3d
smckenzie78 <109680885+smckenzie78@users.noreply.github.com>
2025-06-08 18:14:37 -0400

    Slight polish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,16 @@ export default function Home(){
     return (
         <>
             <div className="flex h-[100vh] w-[100%] flex-col lg:flex-row items-center mt-10 sm:mt-0 sm:justify-center lg:justify-start lg:pl-[30px]">
-                <div className="lg:w-[700px] bg-white lg:h-[95%] w-[200px] h-[200px] relative rounded-full lg:rounded-xl" style={{overflow: 'hidden'}}>
+                <div className="lg:w-[700px] bg-white lg:h-[95%] w-[200px] h-[200px] relative rounded-full lg:rounded-xl border-[3px] border-[#565656] lg:border-0" style={{overflow: 'hidden'}}>
                     <img className="object-cover h-[100%] w-[100%]"
                         src="./meTemp.jpeg"
                         loading="lazy"
                         alt="hero"
                     />
                 </div>
-                <div className=" lg:w-[800px] w-full px-10 flex flex-col items-center lg:items-start lg:gap-7 gap-5 lg:pr-50 lg:ml-50">
+                <div className=" lg:w-[800px] w-full px-10 flex flex-col items-center mt-10 lg: mt-0 lg:items-start lg:gap-7 gap-5 lg:pr-50 lg:ml-50">
                     <h1 className={` uppercase text-center lg:text-right lg:leading-15 font-[700] lg:text-[46px] text-[32px]`}>
-                        <span className="text-cyan-600">— I'm Sacha McKenzie</span>
+                        <span className="text-cyan-600"><span className="lg:inline hidden">— </span>I'm Sacha McKenzie</span>
                         <br/>
                         Software Developer
                     </h1>
